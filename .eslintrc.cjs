71da78a4710ea76f54fdac9619cf6cd3058345e9
gitchaell <f.michaell.a.m@gmail.com>
2025-07-16 17:15:58 -0400

    chore: configure prettier and eslint for Astro with improved code formatting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,40 @@
 module.exports = {
-	env: { es2021: true },
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+	env: { es2021: true, node: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:astro/recommended',
+		'prettier',
+	],
+	overrides: [
+		{
+			files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+			extends: ['plugin:@typescript-eslint/recommended'],
+			rules: {
+				'@typescript-eslint/triple-slash-reference': 'off',
+			},
+		},
+		{
+			files: ['**/*.astro'],
+			parser: 'astro-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser',
+				extraFileExtensions: ['.astro'],
+			},
+			rules: {},
+		},
+	],
 	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
 	plugins: ['@typescript-eslint'],
-	rules: {
-		'@typescript-eslint/triple-slash-reference': 'off',
+	settings: {
+		'import/resolver': {
+			node: {
+				extensions: ['.js', '.jsx', '.ts', '.tsx', '.astro'],
+			},
+		},
 	},
 }
